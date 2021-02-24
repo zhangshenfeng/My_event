de2eb2ec61b8ea5c6abc382e24c23476074bf547
xiao1 <2327793332@qq.com>
2021-02-24 16:26:43 +0800

    提交代码

--- assets/js/API.js
+++ assets/js/API.js
@@ -2,4 +2,23 @@ $.ajaxPrefilter(function (options) {
    
     options.url = "http://ajax.frontend.itheima.net/api" + options.url
     console.log(options.url);
+<<<<<<< Updated upstream
+=======
+    if (options.url.includes("/my")) { 
+        options.headers= {
+            Authorization:localStorage.broken,
+        }
+    };
+    options.complete=function (response) { 
+        // console.log(response);
+
+        //解构
+        const { message, status } = response.responseJSON;
+        // console.log(response.responseJSON);
+        if (message==="身份认证失败！" && status===1) { 
+            localStorage.removeItem("broken");
+            location.href="/login.html"
+        }
+    }
+>>>>>>> Stashed changes
 })
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+<<<<<<< Updated upstream
+=======
+$(function () { 
+    getUserInfo();
+      // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function () {
+      const layer=layui.layer
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+      //do something
+      // 1. 清空本地存储中的 broken
+      localStorage.removeItem('broken')
+      // 2. 重新跳转到登录页面
+      location.href = '/login.html'
+
+      // 关闭 confirm 询问框
+      layer.close(index)
+    })
+  })
+})
+function getUserInfo() { 
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+       
+        success(res) { 
+            // console.log(res);
+            if (res.status !==0) { 
+                return layer.msg("res.message")
+            }
+            rendatavator(res.data)
+        },
+        err() { },
+       
+    })
+};
+function rendatavator(user) {
+    // console.log(user);
+    const username = user.nickname || user.username
+    $("#welcome").html(username)
+    if (user.user_pic) {
+        $(".layui-nav-img").attr("src", user.user_pic).show()
+        $(".text-avatar").hide()
+    } else { 
+        $(".layui-nav-img").hide();
+        const firstName = username[0].toUpperCase()
+        $(".text-avatar").html(firstName).show()
+    }
+}
+>>>>>>> Stashed changes
