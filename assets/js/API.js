aad5c8e578d03d3d63cd51ea1afe3f5a5a1bba95
xiao1 <2327793332@qq.com>
2021-02-24 09:18:33 +0800

    555

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,5 +1,21 @@
 $.ajaxPrefilter(function (options) { 
    
-    options.url = "http://ajax.frontend.itheima.net/api" + options.url
+    options.url = "http://ajax.frontend.itheima.net" + options.url
     console.log(options.url);
+    if (options.url.includes("/my")) { 
+        options.headers= {
+            Authorization:localStorage.broken,
+        }
+    };
+    options.complete=function (response) { 
+        console.log(response);
+
+        //解构
+        const { message, status } = response.responseJSON;
+        // console.log(response.responseJSON);
+        if (message==="身份认证失败！" && status===1) { 
+            localStorage.removeItem("broken");
+            location.href="/login.html"
+        }
+    }
 })
